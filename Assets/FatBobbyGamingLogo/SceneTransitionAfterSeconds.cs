dac611ce1695d4b4527b5ef71ebca4af7a5b2daf
Dan Flanagan <dan.flanagan93@gmail.com>
2016-07-11 20:21:50 -0400

    added fat bobby logo and updated some build settings

---
+++ Assets/FatBobbyGamingLogo/SceneTransitionAfterSeconds.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using FlipWebApps.BeautifulTransitions.Scripts.Transitions;
+using FlipWebApps.BeautifulTransitions.Scripts.Transitions.Screen;
+using UnityEngine.SceneManagement;
+
+public class SceneTransitionAfterSeconds : MonoBehaviour {
+
+    public float TransitionAfter;
+    public string sceneName;
+
+	// Update is called once per frame
+	void Update () {
+        TransitionAfter -= Time.deltaTime;
+        if(TransitionAfter <= 0)
+        {
+            Transition();
+        }
+	}
+
+    private void Transition()
+    {  
+        this.GetComponent<TransitionManager>().TransitionOutAndLoadScene(sceneName);
+    }
+}
