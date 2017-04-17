452d3f3b13caa95bca42cf839d4de579e0d22fd6
Dan Flanagan <dan.flanagan93@gmail.com>
2017-04-17 00:58:48 -0400

    clear log for ease of debugging each turn

---
+++ Assets/MyScripts/Helpers/ClearLog.cs
@@ -0,0 +1,17 @@
+﻿using UnityEditor;
+using System.Reflection;
+using System;
+
+public static class ClearLog {
+
+
+    public static void ClearLogConsole()
+    {
+
+        Assembly assembly = Assembly.GetAssembly(typeof(SceneView));
+        Type logEntries = assembly.GetType("UnityEditorInternal.LogEntries");
+        MethodInfo clearConsoleMethod = logEntries.GetMethod("Clear");
+        clearConsoleMethod.Invoke(new object(), null);
+    }
+}
+
