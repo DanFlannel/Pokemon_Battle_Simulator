ef6abf0728edac4da3e8cd52d0d5a7b385559a1a
Dan Flanagan <dan.flanagan93@gmail.com>
2017-05-15 13:38:52 -0400

    commenting

--- Assets/MyScripts/API/Attack/MoveResults.cs
+++ Assets/MyScripts/API/Attack/MoveResults.cs
@@ -1,10 +1,33 @@
 ﻿public class MoveResults
 {
+    /// <summary>
+    /// Name of the attack move
+    /// </summary>
     public string name;
+
+    /// <summary>
+    /// Did the attack hit
+    /// </summary>
     public bool hit;
+
+    /// <summary>
+    /// Did the attack crit
+    /// </summary>
     public bool crit;
+
+    /// <summary>
+    /// Did the attacker flinch
+    /// </summary>
     public bool flinched;
+
+    /// <summary>
+    /// Did the attacker fail
+    /// </summary>
     public bool failed;
+
+    /// <summary>
+    /// Damage report
+    /// </summary>
     public move_DmgReport dmgReport;
 
     public MoveResults(string s)
