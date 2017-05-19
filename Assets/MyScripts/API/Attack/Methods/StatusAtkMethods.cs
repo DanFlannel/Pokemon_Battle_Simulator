60469e63ca35c3edd5b2d90c0227031c5e37b205
Dan Flanagan <dan.flanagan93@gmail.com>
2017-05-19 10:48:02 -0600

    heiarchy changes

--- Assets/MyScripts/API/Attack/Methods/StatusAtkMethods.cs
+++ Assets/MyScripts/API/Attack/Methods/StatusAtkMethods.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MoveLibrary : MonoBehaviour {
+public class StatusAtkMethods : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
