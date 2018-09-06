2d9cb9d12bcb8e27f2dbceba5146456841772578
DanFlannel <dan.flanagan93@gmail.com>
2018-09-05 23:38:39 -0400

    starting gif loader

---
+++ Assets/Scripts/GIF/GifLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GifLoader : MonoBehaviour
+{
+    public Image player;
+    public Image opponent;
+
+    // Use this for initialization
+    private void Start()
+    {
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
